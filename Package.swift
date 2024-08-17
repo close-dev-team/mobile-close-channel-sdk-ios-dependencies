eb86a119419da861e098662d4ea49129df31e13a
admin <admin@localhost>
2024-08-17 12:14:30 +0200

    Removed swiftyRSA as it is marked .dynamic by the maintainer.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,6 @@ let package = Package(
         .package(url: "https://github.com/socketio/socket.io-client-swift", from: "16.1.0"),
         .package(url: "https://github.com/Ekhoo/Device.git", from: "3.6.0"),
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
-        .package(url: "https://github.com/TakeScoop/SwiftyRSA", from: "1.7.0"),
         .package(url: "https://github.com/adamcichy/SwiftySound", from: "1.3.0"),
         .package(url: "https://github.com/cezheng/Fuzi", from: "3.1.3"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.8.2")
@@ -34,7 +33,6 @@ let package = Package(
                 .product(name: "SQLite", package: "SQLite.swift"),
                 .product(name: "SnapKit", package: "SnapKit"),
                 .product(name: "SocketIO", package: "socket.io-client-swift"),
-                .product(name: "SwiftyRSA", package: "SwiftyRSA"),
                 .product(name: "SwiftySound", package: "SwiftySound")
             ]
         )
